fd0b0756201d81bc23b54852d13649d5be35f07e
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-14 22:13:55 +0300

    Fixed the transition time

--- Components/Word.jsx
+++ Components/Word.jsx
@@ -5,7 +5,7 @@ export default function Word({current, tries, character, color, index}){
 
     console.log(index)
     return (
-        <div style={{ transitionDelay: `${50 * (index + 1)}ms` }} className={`border  p-4 w-full h-16 flex items-center justify-center  
+        <div style={{ transitionDelay: `${150 * (index + 1)}ms` }} className={`border  p-4 w-full h-16 flex items-center justify-center  
            transition-colors
            
            ${ (color === "green" && tries > current) ? 'bg-green-500':''}
